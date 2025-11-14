14fcc7c6970249e318ba6d40a9745c0272516d57
Lucas <lucas.rosenorn@outlook.dk>
2025-11-14 22:18:38 +0100

    Task: Fix build scripts (#21)

--- AuthService/Migrations/20251114211553_InitialCreate.Designer.cs
+++ AuthService/Migrations/20251114211553_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthService.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251111190453_initialCreate")]
-    partial class initialCreate
+    [Migration("20251114211553_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -59,6 +59,9 @@ namespace AuthService.Migrations
 
                     b.Property<int>("Gender")
                         .HasColumnType("int");
+
+                    b.Property<bool>("IsDisabled")
+                        .HasColumnType("bit");
 
                     b.Property<bool>("IsMitIdVerified")
                         .HasColumnType("bit");
